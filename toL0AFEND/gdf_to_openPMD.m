4376eec06baabfe6243d168732081c2916f58331
Sanjeev Chauhan <sanjeev.chauhan@duke.edu>
2023-11-04 13:15:01 -0400

    Update gdf_to_openPMD.m

--- toL0AFEND/gdf_to_openPMD.m
+++ toL0AFEND/gdf_to_openPMD.m
@@ -1,23 +1,26 @@
 function gdf_to_openPMD(gdfFilePath, openPMDOutputPath)
-    % Check for Python version setup in MATLAB
-    disp('Checking for Python venv...')
+    % Checking for Python 3.9 environment setup in MATLAB
+    disp('Checking for Python 3.9 venv...')
     currentPythonEnv = pyenv;
-    if isempty(currentPythonEnv.Version)  % If no Python environment is setup in MATLAB
-        % Provide instructions for setting up a Python environment
-        disp('Python environment is not set up in MATLAB.');
-        disp('Please follow these instructions to set up a Python 3.10 virtual environment:');
+    requiredPythonVersion = '3.9';
+    
+    % Check if Python is set up and if the version is 3.9
+    if isempty(currentPythonEnv.Version) || ~contains(currentPythonEnv.Version, requiredPythonVersion)
+        % Provide instructions for setting up a Python 3.9 environment
+        disp('Python 3.9 environment is not set up in MATLAB.');
+        disp('Please follow these instructions to set up a Python 3.9 virtual environment:');
         disp('1. Open a terminal or command prompt in this directory.');
-        disp('2. Run "python3.10 -m venv gdf_to_pmd" to create a virtual environment named "gdf_to_pmd".');
+        disp('2. Run "python3.9 -m venv gdf_to_pmd" to create a virtual environment named "gdf_to_pmd".');
         disp('3. Activate the virtual environment:');
         disp('   - On Windows, run "gdf_to_pmd\Scripts\activate"');
         disp('   - On macOS/Linux, run "source gdf_to_pmd/bin/activate"');
         disp('4. Install the required Python packages by running "pip install -r requirements.txt".');
-        disp('5. Link the Python executable to MATLAB using "pyenv(''Version'', ''<path-to-gdf_to_pmd-python-executable>'')".');
+        disp('5. Link the Python 3.9 executable to MATLAB using "pyenv(''Version'', ''<path-to-gdf_to_pmd-python-executable>'')".');
         disp('For example, if you are on Windows and you have created the "gdf_to_pmd" environment in the current directory, run:');
         disp('pyenv(''Version'', ''.\gdf_to_pmd\Scripts\python.exe'')');
         disp('If you are on macOS or Linux, run:');
         disp('pyenv(''Version'', ''./gdf_to_pmd/bin/python'')');
-        disp('After setting up the Python environment, rerun the function.');
+        disp('After setting up the Python 3.9 environment, rerun the function.');
         return;
     end
     
